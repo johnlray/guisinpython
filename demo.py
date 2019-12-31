474d465dbef3fceb2bf39ac2885c94529e7819e0
John Ray <j.logan.ray@gmail.com>
2019-12-31 15:18:06 -0800

    the buttons i wanted

--- demo.py
+++ demo.py
@@ -13,15 +13,23 @@ sg.LOOK_AND_FEEL_TABLE['theme_percy'] = {'BACKGROUND': '#DAD0CE',
 
 sg.change_look_and_feel('theme_percy')
 
+user_typeface='Raleway 18'
+
 layout = [
         [sg.Text('Pleae enter a variable label, text, and punches', font='Raleway 20')],
-        [sg.Text('Variable label', size=(30, 1), font='Raleway 20'), sg.InputText()],
-        [sg.Text('Item text', size=(30, 1), font='Raleway 20'), sg.InputText()],
-        [sg.Text('What type of\nquestion is it?', size=(30,2), font='Raleway 20'), sg.InputText()],
+        [sg.Text('Variable label', size=(10, 1), font=user_typeface), sg.InputText(font=user_typeface)],
+        [sg.Text('Item text', size=(10, 1), font=user_typeface), sg.InputText(font=user_typeface)],
+        [sg.Text('What type of\nquestion is it?', size=(10,2), font=user_typeface), sg.InputText(font=user_typeface)],
+        [sg.Submit('Submit'), sg.Cancel()]
         ]
 
 window = sg.Window('Survey item entry window', layout, resizable=True, grab_anywhere=True, size = (600,600))
 
-event, values = window.read()
+while True:
+    event, values = window.read()
+    print(event, values)
+    if event in (None,'Exit'):
+        break
+
 window.close()
 print(event, values[0], values[1], values[2])
