7fc7661cd3b068ac190095f9ba16fbb5c2ca7a4e
John Ray <j.logan.ray@gmail.com>
2020-01-03 22:22:24 -0800

    setup for multi button framework

--- demo.py
+++ demo.py
@@ -15,16 +15,20 @@ sg.change_look_and_feel('theme_percy')
 
 user_typeface='Raleway 18'
 
+single_window_active = False
+multi_window_active = False
+openend_window_active = False
+dyngrid_window_active = False
+ab_randomization_window_active = False
+abc_randomization_window_active = False
+
 layout = [
         [sg.Text('Pleae enter a variable label, text, and punches', font='Raleway 20')],
         [sg.Text('Variable label', size=(10, 1), font=user_typeface), sg.InputText(font=user_typeface)],
         [sg.Text('Item text', size=(10, 1), font=user_typeface), sg.InputText(font=user_typeface)],
-        [sg.Text('What type of\nquestion is it?', font=user_typeface), sg.Radio('Single', 'C1', font=user_typeface), sg.Radio('Multiple', 'C1', font=user_typeface), sg.Radio('Open', 'C3', font=user_typeface), sg.Radio('Open', 'Dyngrid', font=user_typeface)],
-        [sg.Text('Randomizations', font=user_typeface), sg.Radio('None', 'C1', font=user_typeface), sg.Radio('One', 'C1', font=user_typeface), sg.Radio('Two', 'C3', font=user_typeface)],
+        [sg.Text('What type of\nquestion is it?', font=user_typeface), sg.Radio('Single', 'C1', font=user_typeface), sg.Radio('Multiple', 'C1', font=user_typeface), sg.Radio('Open', 'C2', font=user_typeface), sg.Radio('Open', 'C3', font=user_typeface), sg.Radio('Message test with A/B split', 'C4', font=user_typeface), sg.Radio('Message test with A/B/C split', 'C5', font=user_typeface)],
         [sg.Submit('Submit'), sg.Cancel()]
         ]
-
-# https://www.youtube.com/watch?v=Y0Jnyl7aM9g
 
 window = sg.Window('Survey item entry window', layout, resizable=True, grab_anywhere=True, size = (600,300))
 
