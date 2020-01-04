4b74550dbc3b226a55fc5f0cb3eb4aadb7be2c59
John Ray <j.logan.ray@gmail.com>
2020-01-03 22:30:56 -0800

    multirow setup

--- demo.py
+++ demo.py
@@ -24,13 +24,13 @@ abc_randomization_window_active = False
 
 layout = [
         [sg.Text('Pleae enter a variable label, text, and punches', font='Raleway 20')],
-        [sg.Text('Variable label', size=(10, 1), font=user_typeface), sg.InputText(font=user_typeface)],
-        [sg.Text('Item text', size=(10, 1), font=user_typeface), sg.InputText(font=user_typeface)],
-        [sg.Text('What type of\nquestion is it?', font=user_typeface), sg.Radio('Single', 'C1', font=user_typeface), sg.Radio('Multiple', 'C1', font=user_typeface), sg.Radio('Open', 'C2', font=user_typeface), sg.Radio('Open', 'C3', font=user_typeface), sg.Radio('Message test with A/B split', 'C4', font=user_typeface), sg.Radio('Message test with A/B/C split', 'C5', font=user_typeface)],
+        [sg.Text('What type of\nquestion do you want to write?', font=user_typeface)],
+        [sg.Radio('Single', 'Launch single window', font=user_typeface), sg.Radio('Multiple', 'Launch multi window', font=user_typeface), sg.Radio('Open', 'Launch openend window', font=user_typeface)],
+        [sg.Radio('Dyngrid', 'Launch dyngrid window', font=user_typeface), sg.Radio('Message test with A/B split', 'Launch a/b split window', font=user_typeface), sg.Radio('Message test with A/B/C split', 'Launch a/b/c split window', font=user_typeface)],
         [sg.Submit('Submit'), sg.Cancel()]
         ]
 
-window = sg.Window('Survey item entry window', layout, resizable=True, grab_anywhere=True, size = (600,300))
+window = sg.Window('Survey item entry window', layout, resizable=True, grab_anywhere=True, size = (700,300))
 
 while True:
     event, values = window.read()
