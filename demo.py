86e43262fe187f16b43557ebf976d92cee77932a
John Ray <j.logan.ray@gmail.com>
2020-01-09 22:17:13 -0800

    change button type to launch second window

--- demo.py
+++ demo.py
@@ -25,9 +25,9 @@ abc_randomization_window_active = False
 layout = [
         [sg.Text('Pleae enter a variable label, text, and punches', font='Raleway 20')],
         [sg.Text('What type of question do you want to write?', font=user_typeface)],
-        [sg.Radio('Single', 'Launch single window radio', font=user_typeface), sg.Radio('Multiple', 'Launch multi window', font=user_typeface), sg.Radio('Open', 'Launch openend window', font=user_typeface)],
-        [sg.Radio('Dyngrid', 'Launch dyngrid window', font=user_typeface), sg.Radio('Message test with A/B split', 'Launch a/b split window', font=user_typeface), sg.Radio('Message test with A/B/C split', 'Launch a/b/c split window', font=user_typeface)],
-        [sg.Submit('Single', key='Launch single window'), sg.Cancel()]
+        #[sg.Radio('Single', 'SINGLE1', font=user_typeface), sg.Radio('Multiple', 'MULTI1', font=user_typeface), sg.Radio('Open', 'OPEN1', font=user_typeface)],
+        #[sg.Radio('Dyngrid', 'DYNGRID1', font=user_typeface), sg.Radio('Message test with A/B split', 'AB1', font=user_typeface), sg.Radio('Message test with A/B/C split', 'ABC1', font=user_typeface)],
+        [sg.Button('Single', key='SINGLE1'), sg.Cancel()]
         ]
 
 window = sg.Window('Survey item entry window', layout, resizable=True, grab_anywhere=True, size = (700,300))
@@ -35,21 +35,15 @@ window = sg.Window('Survey item entry window', layout, resizable=True, grab_anyw
 i=0
 
 while True:
-    event, values = window.read()
+    event, values = window.read(timeout=100)
+    #if event != sg.TIMEOUT_KEY:
+    #    print(i, event, values)
     if event in (None,'Exit'):
         break
+    #elif event == 'SINGLE1':
+    #    sg.popup('This is a BLOCKING popup','all windows remain inactive while popup active')
     i+=1
-    if event == 'Launch single window' and not single_window_active:
-        single_window_active = True
-        single_window_layout = [
-                [sg.Text('Single-response item')],
-                [sg.Text('Variable label: ', font=user_typeface), sg.Input(font=user_typeface)],
-                [sg.Text('Item text', font=user_typeface), sg.Input(font=user_typeface)]
-                [sg.Text('What type of item is it?')],
-                [sg.Radio('Support/oppose', font=user_typeface), sg.Radio('Approve/disapprove', font=user_typeface), sg.Radio('Agree/disagree', font=user_typeface)]
-                ]
-        single_window = sg.Window('Single-response item window', single_window_layout)
-    if event == 'Launch multi window' and not single_window_active:
+    if event == 'SINGLE1' and not single_window_active:
         single_window_active = True
         single_window_layout = [
                 [sg.Text('Single-response item')],
